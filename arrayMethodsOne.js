d3d55fe8a92281ddf5c77af3d16b07b5ef16b1b8
Jacob Spackman <jacobts202@gmail.com>
2023-03-23 20:33:18 -0600

    1-4 complete

--- arrayMethodsOne.js
+++ arrayMethodsOne.js
@@ -19,9 +19,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 
 // CODE HERE
 const evenNumbers = mixedNumbers.filter((e, i, a) => {
-  return e / 2 === 0
+  return e % 2 === 0
 })
-console.log(evenNumbers)
+// console.log(evenNumbers)
 
 
 
@@ -42,8 +42,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+const postTaxPrices = prices.map((e, i, a) => {
+  return e * 1.07
+} );
+console.log(postTaxPrices)
 
 
 ////////// PROBLEM 3 //////////
@@ -60,7 +62,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation   = populations.reduce((acc, curr) => {acc + curr})
 
 
 
@@ -85,7 +87,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter((element) => {
+  return element.CP > 200
+})
 
 
 
